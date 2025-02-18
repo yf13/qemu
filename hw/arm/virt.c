03478c5bdfeeb290f51a7113c0795f99ee2ff24e
Yanfeng Liu <yfliu2008@qq.com>
2025-02-18 14:20:18 +0800

    arm/virt: add cortex-r52
    This allows one to use `-cpu cortex-r52` with `-M virt` for
    qemu-system-arm command.
    
    Signed-off-by: Yanfeng Liu <yfliu2008@qq.com>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -3084,6 +3084,7 @@ static void virt_machine_class_init(ObjectClass *oc, void *data)
     HotplugHandlerClass *hc = HOTPLUG_HANDLER_CLASS(oc);
     static const char * const valid_cpu_types[] = {
 #ifdef CONFIG_TCG
+        ARM_CPU_TYPE_NAME("cortex-r52"),
         ARM_CPU_TYPE_NAME("cortex-r5f"),
         ARM_CPU_TYPE_NAME("cortex-a7"),
         ARM_CPU_TYPE_NAME("cortex-a15"),
